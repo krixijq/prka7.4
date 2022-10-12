433af578583598fa2d9d2d408c7888b523909144
WdariaW9 <talankina03@bk.ru>
2022-10-12 11:14:59 +0300

    lrk7.4.1

--- w/w/urls.py
+++ w/w/urls.py
@@ -22,6 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('users/', include('django.contrib.auth.urls')),
-    path('articles/', include('article.urls')),
+    path('articles/', include('articles.urls')),
 
 ]
